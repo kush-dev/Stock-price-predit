6b5e5db613b5a90aaa782e7f7e51fd36d2660134
Acidic Train <60462266+kush-dev@users.noreply.github.com>
2023-03-07 12:17:58 +0300

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -15,7 +15,7 @@ from datetime import datetime
 
 accuracy = random.uniform(88,96)
 start_date = st.sidebar.date_input("Start date", datetime(2022,10, 25))
-end_date = st.sidebar.date_input("End date", datetime(2023,3,5))
+end_date = st.sidebar.date_input("End date: Do not Modify Date is upto Current stream", datetime(2023,3,5))
 total_tasks = 100
 
 progress_bar = st.progress(0)
